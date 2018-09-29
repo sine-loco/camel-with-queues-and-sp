5566b4e42797506847df7c23b7a1e940df8f245f
sine loco <ladimir@gmail.com>
2018-09-29 03:14:38 +0300

    first stable build, yet it does nothing

---
+++ src/main/java/ru/snm/misc/camel/camel_with_queues_and_sp/CamelWithQueuesAndSpApplication.java
@@ -0,0 +1,12 @@
+package ru.snm.misc.camel.camel_with_queues_and_sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelWithQueuesAndSpApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run( CamelWithQueuesAndSpApplication.class, args );
+    }
+}
---
+++ src/test/java/ru/snm/misc/camel/camel_with_queues_and_sp/CamelWithQueuesAndSpApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.snm.misc.camel.camel_with_queues_and_sp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CamelWithQueuesAndSpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
